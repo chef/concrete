28058bf5da002cd1a3cd51b03d57e61b985f868d
Seth Falcon <seth@opscode.com>
2014-05-07 22:07:19 -0700

    concrete init now creates the directory for you
    The new command line is: concrete init DIR
    
    This further streamlines the interaction since the DIR arg is the
    project name. So concrete only has to ask if you want an active
    application.

--- src/concrete.erl
+++ src/concrete.erl
@@ -31,41 +31,45 @@
          main/1
         ]).
 
-main(["init"]) ->
-    concrete_init();
+main(["init", Dir]) ->
+    concrete_init(Dir);
 main(["update"]) ->
     concrete_update();
 main(_Args) ->
     io:format("Sorry, you'll have to call me like one of the examples below:\n"
-              "concrete init\n"
+              "concrete init DIR\n"
               "concrete update\n"),
     halt(1).
 
-concrete_init() ->
-    verify_empty_directory(),
+concrete_init(Dir) ->
+    create_directory(Dir),
     verify_rebar(),
-    io:format("Initialize a new project with concrete\n\n"),
-    Name = strip(io:get_line("Project name: ")),
-    ActiveApp = yes_no(io:get_line("Active application? (y/n): ")),
+    io:format("Creating the ~s project with concrete\n\n", [Dir]),
+    Name = strip(Dir),
+    ActiveApp = yes_no(io:get_line("Would you like an active application? (y/n): ")),
     render_project(Name),
     render_active(Name,ActiveApp),
-    io:format("Now try: make\n"),
+    io:format("Now try: cd ~s; make\n", [Dir]),
     ok.
 
 render_project(Name) ->
     CmdFmt = "rebar create template_dir=~s template=concrete_project name=~s",
     Cmd = io_lib:format(CmdFmt, [template_dir(), Name]),
-    io:format("Creating ~s via '~s'~n", [Name, Cmd]),
-    os:cmd(Cmd).
+    cmd_in_dir(Cmd, Name).
     
 render_active(Name, true) ->
     CmdFmt = "rebar create --force template_dir=~s template=concrete_app name=~s",
     Cmd = io_lib:format(CmdFmt, [template_dir(), Name]),
-    io:format("Creating ~s via '~s'~n", [Name, Cmd]),
-    os:cmd(Cmd);
+    cmd_in_dir(Cmd, Name);
 render_active(_Name, _) ->
     ok.
 
+cmd_in_dir(Cmd, Dir) ->
+    {ok, CWD} = file:get_cwd(),
+    file:set_cwd(Dir),
+    Res = os:cmd(Cmd),
+    file:set_cwd(CWD),
+    Res.
     
 verify_rebar() ->
     case os:find_executable("rebar") of
@@ -76,13 +80,13 @@ verify_rebar() ->
             ok
     end.
 
-verify_empty_directory() ->
-    case filelib:wildcard("*") of
-        [] ->
-            ok;
-        Files ->
-            io:format("ERROR: concrete init wants an empty directory\n"
-                      "       But we found: ~p\n", [Files]),
+create_directory(Dir) ->
+    case filelib:is_file(Dir) of
+        false ->
+            filelib:ensure_dir(filename:join(Dir, "stub"));
+        true ->
+            io:format("ERROR: concrete init wants to create '~s', but it already exists\n",
+                      [Dir]),
             halt(1)
     end.
 
@@ -145,7 +149,7 @@ template_dir() ->
 concrete_dir() ->
     %% Note that code:which and code:priv_dir return odd looking
     %% results when run via an escriptized.
-    filename:dirname(escript:script_name()).
+    filename:absname(filename:dirname(escript:script_name())).
 
 yes_no(S) ->
     case hd(string:to_lower(strip(S))) of
